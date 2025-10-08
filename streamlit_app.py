c4e26bbf3f830fa3ef7b321639ffefe65466f5f3
DLAIE Burner <dlaieburner@gmail.com>
2025-10-08 17:27:28 -0500

    adjusted logos

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,7 +103,16 @@ def display_leaderboard(use_streamlit=True):
 
     st.markdown("---")
     st.markdown("### With prizes from:")
-    st.markdown('<img src="https://raw.githubusercontent.com/dlaieburner/2025-leaderboard/refs/heads/main/bdaic_logo.jpg" height="100"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <img src="https://raw.githubusercontent.com/dlaieburner/2025-leaderboard/refs/heads/main/wandb_logo.png" height="100"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <img src="https://raw.githubusercontent.com/dlaieburner/2025-leaderboard/refs/heads/main/coreweave_logo.jpg" height="100">', unsafe_allow_html=True)
+
+    st.markdown('''
+    <div style="display: flex; justify-content: space-between; align-items: center;">
+        <img src="https://raw.githubusercontent.com/dlaieburner/2025-leaderboard/refs/heads/main/bdaic_logo.png" height="100">
+        <img src="https://raw.githubusercontent.com/dlaieburner/2025-leaderboard/refs/heads/main/wandb_logo.png" height="100">
+        <img src="https://raw.githubusercontent.com/dlaieburner/2025-leaderboard/refs/heads/main/coreweave_logo.jpg" height="100">
+    </div>
+    ''', unsafe_allow_html=True)
+
+
     return df_display
 
 if __name__ == "__main__":
