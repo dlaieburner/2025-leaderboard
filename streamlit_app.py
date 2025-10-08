0498647922366d4d7a0876b2ed94932c44b4a3e7
DLAIE Burner <dlaieburner@gmail.com>
2025-10-08 07:19:22 -0500

    trying separators

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,11 @@ from datetime import datetime
 import argparse
 
 def style_leaderboard(df_display):
-    df_display = df_display.style.set_properties(**{'border-left': '3px solid #666'}, subset=['Total Params ↓', 'MSE ↓', 'Entropy ↓', 'Overall Score ↓'])
-    df_disply =  df_display.style.set_properties(**{'background-color': 'darkgray'}, subset=['MSE ↓', 'SSIM ↑'])
-    return df_display
+    return df_display.style \
+        .set_properties(**{'border-left': '3px solid #666'},
+                       subset=['Total Params ↓', 'MSE ↓', 'Entropy ↓', 'Overall Score ↓']) \
+        .set_properties(**{'background-color': 'darkgray'},
+                       subset=['MSE ↓', 'SSIM ↑'])
 
 
 def display_leaderboard(use_streamlit=True):
