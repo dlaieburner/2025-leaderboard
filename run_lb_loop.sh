88889e00492044aa409d5ea74455a972807320b9
DLAIE Burner <dlaieburner@gmail.com>
2025-10-09 14:36:25 -0500

    updated filename

--- run_lb_loop.sh
+++ run_lb_loop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true; do
   echo "===== Running at $(date) ====="
-  ../botograder/scripts/lb_canvas.py `cat canvas_assignment_url.txt` lb_test
+  ../botograder/scripts/lb_canvas.py `cat CANVAS_ASSIGNMENT_URL.txt` lb_test
   echo "Sleeping 20 minutes..."
   sleep 1200
 done
